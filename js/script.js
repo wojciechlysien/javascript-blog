e0009a18e214dbff5864390bcebbefbefc472c6b
Wojciech Lysien <wojciechlysien@gmail.com>
2019-11-10 17:32:19 +0000

    remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -4,13 +4,23 @@
 //   console.log('links:', links);
 // });
 const titleClickHandler = function (event) {
-  console.log(event);
+  console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.active');
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
